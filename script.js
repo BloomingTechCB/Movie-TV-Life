27424f2df2d04d071b3da82f2b72c5826643f607
BloomingTechCB <borthwickcaitlin@gmail.com>
2025-06-15 18:55:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-console.log("Theme toggle script loaded.");
+onsole.log("Theme toggle script loaded.");
 
+// Toggle Light/Dark Mode
 document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('.toggle-theme');
   if (button) {
